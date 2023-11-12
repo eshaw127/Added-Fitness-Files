81018bfefdffedcd0f9e5a6ee515cd8eb4c535d6
eshaw127 <142946718+eshaw127@users.noreply.github.com>
2023-11-12 01:56:17 -0600

    Add files via upload

---
+++ media/scripts/script.js
@@ -0,0 +1,18 @@
+/*
+Student Name: Elizabeth Shaw
+File Name: script.js
+Date: 11/07/2023
+*/
+
+//Hamburger menu
+hamburger() {
+	var menu = document.getElementById("menu-links");
+	var logo = document.getElementById("ffc-logo");
+	if (menu.style.display === "block" && logo.style.display === "none") {
+		menu.style.display = "none";
+		logo.style.display = "block";
+	} else {
+		menu.style.display = "block";
+		logo.style.display = "none";
+	}
+}
